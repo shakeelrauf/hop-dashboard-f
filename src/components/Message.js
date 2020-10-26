cf32ec2cbdf24174b3547dd9bc9d1a1c7215f270
Forrest Carlton <forrestiscarlton@gmail.com>
2020-10-26 12:18:56 +0500

    Remove antd package.

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,193 +1,48 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { FlagOutlined } from '@material-ui/icons';
-import {
-  Comment,
-  message as msgNotify,
-} from 'antd';
-import { Modal, Tooltip, Badge, Avatar } from '@material-ui/core';
-import moment from 'moment';
-import { Player } from 'video-react';
-import { Linkify } from './common';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
-const getTimestamp = message => {
-  return (new Date());
-};
-  
-class Message extends Component {
-    
-  state = {
-    flagged: false
-  };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+}));
 
-  componentDidMount() {
-    const { message, markAsRead, user } = this.props;
-    if (!message.read && markAsRead && message.sender.uuid !== user.uuid) {
-      this.timeout = setTimeout(() => {
-        markAsRead(message.uuid);
-      }, 5000);
-    }
-  }
+export default function AlignItemsList() {
+  const classes = useStyles();
 
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
-  }
-
-  getType(message) {
-    let { type } = message;
-
-    if (
-      (message.type === 'message' ||
-        message.type === 'text' ||
-        message.type === 'communication' ||
-        message.type === 'group') &&
-      message.file &&
-      (message.file.object || message.object)
-    ) {
-      type = message.file.object || message.object;
-    }
-
-    return type;
-  }
-
-  getActions() {
-    const { message, currentContact, user } = this.props;
-    if (user.role !== 'administrator') {
-      return [];
-    }
-
-    if (currentContact.type.toLowerCase() === 'group') {
-      return [
-        <Tooltip title="Flag">
-          <FlagOutlined
-            onClick={() => this.flagMessage(message, currentContact)}
-          />
-        </Tooltip>
-      ];
-    }
-
-    return [];
-  }
-
-  flagMessage = (message, currentContact) => {
-    Modal.confirm({
-      title: 'Are you sure you want to flag this message?',
-      okText: 'Yes',
-      okType: 'danger',
-      cancelText: 'No',
-      onOk: async () => {
-        // const func = message.type === 'journal' ? 'flagJournal' : 'flagMessage';
-        const resp = {ok: true};
-        if (!resp.ok) {
-          msgNotify.error(resp.data.message || 'There was problem flagging');
-        } else {
-          msgNotify.success('The message was flagged successfully');
-          this.setState({ flagged: true });
-        }
-      }
-    });
-  };
-
-  renderContent(message) {
-    const type = this.getType(message);
-
-    if (type === 'image') {
-      return (
-        <div className="msg">
-          <img className="msg-image" src={message.file.link} alt="message" />
-          {message.message && (
-            <p>
-              <Linkify>{message.message}</Linkify>
-            </p>
-          )}
-        </div>
-      );
-    }
-
-    if (type === 'video') {
-      return (
-        <div className="msg">
-          <div className="msg-image">
-            <Player>
-              <source src={message.file.link} />
-            </Player>
-          </div>
-          {message.message && (
-            <p>
-              <Linkify>{message.message}</Linkify>
-            </p>
-          )}
-        </div>
-      );
-    }
-
-    if (type === 'journal') {
-      return (
-        <div>
-          <p>
-            <b>{`Subject: ${message.subject}`}</b>
-          </p>
-          <p>
-            <Linkify>{message.message}</Linkify>
-          </p>
-        </div>
-      );
-    }
-
-    return (
-      <p>
-        <Linkify>{message.message}</Linkify>
-      </p>
-    );
-  }
-
-  renderDateTime(message) {
-    const time = moment(getTimestamp(message), 'X');
-    return (
-      <Tooltip title={time.format('MM/DD/YYYY h:mm A')}>
-        <span>{time.fromNow()}</span>
-      </Tooltip>
-    );
-  }
-
-  render() {
-    const { message, compact, unreadCount } = this.props;
-    const { flagged } = this.state;
-
-    if (flagged) {
-      return null;
-    }
-
-    return (
-      <Comment
-        className="chat-message"
-        author={<a href='/'>{message.sender.name}</a>}
-        avatar={
-          <Badge count={unreadCount}>
-            <Avatar src={message.sender.avatar} alt={message.name} />
-          </Badge>
-        }
-        content={this.renderContent(message)}
-        datetime={this.renderDateTime(message)}
-        actions={!compact && this.getActions()}
-      />
-    );
-  }
+  return (
+    <List className={classes.root}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+  );
 }
-
-Message.propTypes = {
-  message: PropTypes.object,
-  currentContact: PropTypes.object,
-  user: PropTypes.object,
-  compact: PropTypes.bool,
-  markAsRead: PropTypes.func
-};
-
-const mapStatesToProps = state => {
-  return {
-    user: state.auth.user
-  };
-};
-
-export default connect(mapStatesToProps)(Message);
--- src/layouts/PatientMessageDropdown/index.js
+++ src/layouts/PatientMessageDropdown/index.js
@@ -1,29 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Empty, Menu, Dropdown } from 'antd';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 import MessageOutlinedIcon from '@material-ui/icons/MessageOutlined';
 import Badge from '@material-ui/core/Badge';
 import Message from '../../components/Message';
+import Popper from '@material-ui/core/Popper';
+import Grow from '@material-ui/core/Grow';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
 const PatientMessageDropdown = ({ unread, user, className }) => {
   const [items] = useState([{messages: ['heello'], unread: false, type: 'message'}]);
   const [loading, setLoading] = useState(false);
   const [visible, setVisible] = useState(false);
-  const history = useHistory();
+  const anchorRef = React.useRef(null);
 
+  // const history = useHistory();
+
+  const handleToggle = () => {
+    setVisible((visible) => !visible);
+  };
+
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setVisible(false);
+  };
   const loadMessages = async () => {
     setLoading(true);
     setLoading(false);
   };
 
   const goToChat = item => {
-    history.push(
-      `/chats?uuid=${item.uuid}&type=${
-        item.role === 'user' ? 'patient' : 'contacts'
-      }`
-    );
+    // history.push(
+    //   `/chats?uuid=${item.uuid}&type=${
+    //     item.role === 'user' ? 'patient' : 'contacts'
+    //   }`
+    // );
   };
 
   useEffect(() => {
@@ -33,48 +52,54 @@ const PatientMessageDropdown = ({ unread, user, className }) => {
   }, [visible]);
 
   const menu = (
-    <Menu className="layout-header-nav__menu">
+    <MenuList>
       {!loading && items.length === 0 && (
-        <Menu.Item disabled>
-          <Empty
-            image={Empty.PRESENTED_IMAGE_SIMPLE}
-            description="No new messages"
-          />
-        </Menu.Item>
+        <MenuList disabled>
+            No new messages
+        </MenuList>
       )}
       {!loading &&
         items.length > 0 &&
         items.map((item, index) => (
-          <Menu.Item key={index} onClick={() => goToChat(item)}>
+          <MenuItem key={index} onClick={() => goToChat(item)}>
             <Message
-              message={{
-                ...item.messages[0],
-                sender: item
-              }}
-              unreadCount={item.unread}
-              user={user}
-              compact
+              item={item}
             />
-          </Menu.Item>
+          </MenuItem>
         ))}
-    </Menu>
+    </MenuList>
   );
 
   return (
-    <Dropdown
-      overlay={menu}
-      placement="bottomRight"
-      overlayStyle={{zIndex: 999999999}}
-      trigger={['click']}
-      onVisibleChange={setVisible}
-      visible={visible}
+    <Grid
+      container
+      aria-controls={visible ? 'menu-list-grow' : undefined}
+      aria-haspopup="true"
+      onClick={handleToggle}
+      ref={anchorRef}
+
     >
       <div className="header-notification__trigger">
         <Badge badgeContent={unread} color="secondary">
           <MessageOutlinedIcon className={className} />
         </Badge>
       </div>
-    </Dropdown>
+      <Popper open={visible} anchorEl={anchorRef.current}  role={undefined} transition disablePortal>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{ transformOrigin:  'center bottom' }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+
+                {menu}
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </Grid>
   );
 };
 
